a743db5987af26766202b32425406611561dd4a9
Lee Englestone <lee.englestone@gmail.com>
2024-08-15 18:35:01 +0100

    Improvement to spawning so wont to spawn on trigger press too fast

--- Assets/Scripts/SceneSpawn.cs
+++ Assets/Scripts/SceneSpawn.cs
@@ -1,65 +1,50 @@
 ﻿using Meta.XR.MRUtilityKit;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using static Meta.XR.MRUtilityKit.MRUKAnchor;
 
 namespace Assets.Scripts
 {
     public class SceneSpawn : MonoBehaviour
     {
         public Transform rayStartPoint;
-        public float rayLength = 5;
+        public float rayLength = 6;
         public MRUKAnchor.SceneLabels labelFilter;
+        private GameObject _debugCube;
+        private float lastSpawn;
 
+        [SerializeField]
+        private float cooldown = 0.5f;
 
-        private GameObject _debugCube2;
-        
-      
         void Start()
         {
-            Debug.Log("SceneSpawn.Start");
-
-            _debugCube2 = GameObject.Find("SomeCube");
-
+            _debugCube = GameObject.Find("SomeCube");
         }
-
-       
 
         void Update()
         {
-
-            Debug.Log("SceneSpawn.Update");
             Ray ray = new Ray(rayStartPoint.position, rayStartPoint.forward);
 
             MRUKRoom room = MRUK.Instance.GetCurrentRoom();
             bool hasHit = room.Raycast(ray, rayLength, LabelFilter.FromEnum(labelFilter), out RaycastHit hit, out MRUKAnchor anchor);
 
-            if(hasHit)
+            if (hasHit)
             {
                 Vector3 hitPoint = hit.point;
-                
-                _debugCube2.transform.position =  hitPoint;
-                
+
+                _debugCube.transform.position = hitPoint;
 
                 // If right trigger press.. place a cube at that position
-                if(OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger) ||
+                if (OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger) ||
                     OVRInput.Get(OVRInput.Button.SecondaryHandTrigger))
                 {
-                    GameObject newCube = GameObject.Instantiate(_debugCube2);
+                    if (Time.time < lastSpawn + cooldown)
+                        return;
+
+                    GameObject newCube = GameObject.Instantiate(_debugCube);
                     newCube.transform.position = hitPoint;
 
+                    lastSpawn = Time.time;
                 };
             }
-
-
-         
         }
-
-      
     }
 }
